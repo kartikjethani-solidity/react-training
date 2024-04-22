543d016c8e50f078a416b4a217feed4ccae588c6
Kartik Jethani <kartik.jethani@worksimpli.io>
2024-04-22 15:13:26 +0530

    api creation using json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Profile } from "./components/profile";
 import { User } from "./types/user";
--- src/components/user-listing/user-listing.tsx
+++ src/components/user-listing/user-listing.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, MouseEventHandler, useEffect, useState } from "react";
 import { User } from "../../types/user";
 import { Profile } from "../profile";
 
@@ -12,16 +12,26 @@ export const UserListing: FC<Props> = ({ users }) => {
   // get users data from swapi
   // display name, height, age, hair_color
   // create a neat UI - refer tailwind ui components
+  const updateUser: MouseEventHandler<HTMLButtonElement> = (event) => {
+    const hashedEmail = "xxxx@xyz.com";
+    setEmail(hashedEmail);
+    // const updatedName = "Dhruv";
+    // setName(updatedName);
+  };
+
+  const [email, setEmail] = useState<string>(users[0].email);
+  const [name, setName] = useState<string>(users[0].name);
+
+  // useEffect(() => {
+  //   alert(`${name} ${email}`);
+  // }, [name, email]);
 
   return (
     <>
-      {users.map((item) => {
-        const modifiedUsers = {
-          ...item,
-          //   isLoggedIn: true,
-        };
-        return <Profile {...modifiedUsers} />;
+      {users.map((user) => {
+        return <Profile email={email} name={user.name} id={user.id} />;
       })}
+      <button onClick={updateUser}>Hide Email</button>
     </>
   );
 };
