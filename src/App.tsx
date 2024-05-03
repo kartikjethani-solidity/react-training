8aa9721b229f6a850565e42cdf9153440ac249c4
Harsh Karki <corecorecrorecrore@gmail.com>
2024-05-03 13:57:26 +0530

    form with validation (client side)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Profile } from "./components/profile";
 import { User } from "./types/user";
 import { H1 } from "./components/h1";
 import { UserListing } from "./components/user-listing";
+import { Modal } from "./components/modal";
+import { LoginForm } from "./components/login-form";
 
 function App() {
   // STATE
@@ -12,41 +14,9 @@ function App() {
   const myClassName = isLoggedIn ? "a" : "b";
 
   return (
-    <div className="App">
-      {user ? (
-        <Profile
-          user={user}
-          clickHandler={() => {}}
-          // address={{
-          //   firstLine: "Corenthum",
-          //   city: "Noida",
-          //   state: "UP",
-          // }}
-        />
-      ) : (
-        <button onClick={signIn}>Sign In</button>
-      )}
-
-      <div className={myClassName}>Hello</div>
-
-      {/* <Profile
-        email="shivansh@abc.com"
-        id={2}
-        name="Shivansh"
-        address={{
-          firstLine: "Corenthum",
-          city: "Noida",
-          state: "UP",
-        }}
-      /> */}
-
-      <div style={{ marginTop: "100px" }}>
-        <H1 heading="List of all users" />
-        <div style={{ marginTop: "20px" }}>
-          <UserListing />
-        </div>
-      </div>
-    </div>
+    <Modal title="Login Modal">
+      <LoginForm></LoginForm>
+    </Modal>
   );
 }
 
