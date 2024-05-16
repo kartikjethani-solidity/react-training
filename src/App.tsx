28d01ad6dbf66f2cb9b0d29e02425b950b4e60d6
Dhruv Sharma <dhruv.sharma@worksimpli.io>
2024-05-16 14:24:49 +0530

    login form style

--- src/App.tsx
+++ src/App.tsx
@@ -53,15 +53,15 @@ function App() {
 
   // return <Counter />;
 
-  // return (
-  //   <Modal title="Login Modal">
-  //     <LoginForm>
-  //       <Modal title="Edit Profile Pic">
-  //         <h1></h1>
-  //       </Modal>
-  //     </LoginForm>
-  //   </Modal>
-  // );
+  return (
+    <Modal title="Login Modal">
+      <LoginForm>
+        {/* <Modal title="Edit Profile Pic">
+          <h1></h1>
+        </Modal> */}
+      </LoginForm>
+    </Modal>
+  );
 
   // return (
   //   <div className="App">
@@ -99,29 +99,31 @@ function App() {
   //       </div>
   //     </div>
   //   </div>
-  return (
-    <div className="App">
-      {user ? (
-        <div className="grid grid-cols-3 place-items-center">
-          <Profile user={user} clickHandler={() => {}} />
-        </div>
-      ) : (
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          onClick={signIn}
-        >
-          Signin
-        </button>
-      )}
-      <div style={{ marginTop: "100px" }}>
-        <H1 heading="List of all users" />
-        <div style={{ marginTop: "20px" }}>
-          <UserListing />
-        </div>
-      </div>
-    </div>
-  );
+
+  //--------------TAILWIND CARDS-------------------------
+  // return (
+  //   <div className="App">
+  //     {user ? (
+  //       <div className="grid grid-cols-3 place-items-center">
+  //         <Profile user={user} clickHandler={() => {}} />
+  //       </div>
+  //     ) : (
+  //       <button
+  //         type="button"
+  //         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+  //         onClick={signIn}
+  //       >
+  //         Signin
+  //       </button>
+  //     )}
+  //     <div style={{ marginTop: "100px" }}>
+  //       <H1 heading="List of all users" />
+  //       <div style={{ marginTop: "20px" }}>
+  //         <UserListing />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
